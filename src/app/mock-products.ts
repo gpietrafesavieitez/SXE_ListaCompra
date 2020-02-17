9aac2b8cb9e9cd7acf67b51ec204bbeda042e7d4
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2020-02-17 14:04:08 +0100

    init

---
+++ src/app/mock-products.ts
@@ -0,0 +1,17 @@
+import {Product} from './product';
+
+export const PRODUCTS: Product[] = [
+  { name: 'Pasta', status: false },
+  { name: 'Huevos', status: false },
+  { name: 'Café', status: false },
+  { name: 'Yogur', status: false },
+  { name: 'Hummus', status: false },
+  { name: 'Pizza', status: false },
+  { name: 'Pollo', status: false },
+  { name: 'Carne', status: false },
+  { name: 'Pesto', status: false },
+  { name: 'Atún', status: false },
+  { name: 'Nueces', status: false },
+  { name: 'Cocacola', status: false },
+  { name: 'Cerveza', status: false }
+];
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { PRODUCTS } from '../mock-products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  products = PRODUCTS;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(product: Product) {
+    product.status = !product.status;
+  }
+}
